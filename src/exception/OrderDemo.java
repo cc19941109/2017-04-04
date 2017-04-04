8272afbffafaadb60b9025fe5b19a95ace78481b
cc19941109 <15355498770@163.com>
2017-04-04 19:35:40 +0800

    initial

---
+++ src/exception/OrderDemo.java
@@ -0,0 +1,37 @@
+package exception;
+/*
+ * Error类和Exception类的父类都是throwable类，他们的区别是：
+Error类一般是指与虚拟机相关的问题，如系统崩溃，虚拟机错误，内存空间不足，方法调用栈溢等。对
+于这类错误的导致的应用程序中断，仅靠程序本身无法恢复和和预防，遇到这样的错误，建议让程序终止。
+Exception类表示程序可以处理的异常，可以捕获且可能恢复。遇到这类异常，应该尽可能
+处理异常，使程序恢复运行，而不应该随意终止异常。
+Exception类又分为运行时异常（Runtime Exception）和受检查的异常(Checked Exception )，运行
+时异常;ArithmaticException,IllegalArgumentException，编译能通过，但是一运行就终止了，程序
+不会处理运行时异常，出现这类异常，程序会终止。而受检查的异常，要么用try。。。catch捕获，要么
+用throws字句声明抛出，交给它的父类处理，否则编译不会通过。
+运行时异常是 Exception 的子类，也有一般异常的特点，是可以被 Catch 块处理的。只不过往往我们不
+对他处理罢了。也就是说，你如果不对运行时异常进行处理，那么出现运行时异常之后，要么是线程中止，
+要么是主程序终止。
+如队列里面出现异常数据了，正常的处理应该是把异常数据舍弃，然后记录日志。不应
+该由于异常数据而影响下面对正常数据的处理。
+ * 
+ * */
+
+
+public class OrderDemo {
+
+	public static void main(String[] args) {
+		try {
+			System.out.println("1");
+			int i =10/0;
+			System.out.println("2");
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+		}finally {
+			System.out.println("finally");
+		}
+		System.out.println("5");
+	}
+
+}
--- src/objectDemo/Equals.java
+++ src/objectDemo/Equals.java
@@ -4,6 +4,9 @@ package objectDemo;
 //字符串对象为空和空字符串有区别
 //  ""占内存，在内存中会分配一个空间。
 //null不占内存，为空引用。
+//字符串对象与null的值不相等，且内存地址也不相等；
+//空字符串对象与null的值不相等，且内存地址也不相等；
+//创建一个字符串对象的默认值为""
 
 public class Equals {
 
@@ -13,17 +16,17 @@ public class Equals {
 		String b = null;
 		System.out.println(a.toString());
 	//	System.out.println(b.toString());// java.lang.NullPointerException
-		System.out.println(a.equals(b));
-		System.out.println(a==b);
+		System.out.println(a.equals(b));//false  
+		System.out.println(a==b);//false 
 		
 		String c = "ccc";
 		String d = new String("ccc");
-		System.out.println("c d "+c.equals(d));
-		System.out.println(c==d);
+		System.out.println("c d "+c.equals(d));//c d true 相同内容
+		System.out.println(c==d);//false  不同地址
 
-		String e  = new String();
-		System.out.println(e.equals(a));
-		System.out.println(e.toString());//new String -->""
+		String e  = new String();//默认String
+		System.out.println(e.equals(a));//true
+		System.out.println(e.toString());//输出为空，即  e = "";
 	}
 
 }
